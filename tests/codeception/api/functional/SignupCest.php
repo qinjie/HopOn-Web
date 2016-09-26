28d95a1bb1c3d366858df3876d470d78390136b9
canhnht <canhnht1709@gmail.com>
2016-09-26 15:39:14 +0800

    Add test user API

--- tests/codeception/api/functional/SignupCest.php
+++ tests/codeception/api/functional/SignupCest.php
@@ -4,12 +4,15 @@ use tests\codeception\api\FunctionalTester;
 
 class SignupCest
 {
+    private $accessToken;
+
     public function _before(FunctionalTester $I)
     {
     }
 
     public function _after(FunctionalTester $I)
     {
+
     }
 
     public function testSignup(FunctionalTester $I)
@@ -23,7 +26,58 @@ class SignupCest
             'role' => 10
         ]);
         $I->seeResponseCodeIs(200);
+        $response = json_decode($I->grabResponse());
+        $I->assertEquals($response, 'register successfully');
+
+        $I->seeInDatabase('user', [
+            'fullname' => 'Tan Ah Boy',
+            'role' => 10,
+            'status' => 2,
+        ]);
+        $userId = $I->grabFromDatabase('user', 'id', [
+            'email' => '123@mail.com',
+        ]);
+        $I->seeInDatabase('user_token', [
+            'user_id' => $userId,
+            'action' => 1,
+        ]);
     }
 
+    public function testResendActivationEmail(FunctionalTester $I) {
+        $I->wantTo('resend activation email');
+        $I->sendPOST('user/resend-email', [
+            'email' => '123@mail.com'
+        ]);
+        $I->seeResponseCodeIs(200);
+        $response = json_decode($I->grabResponse());
+        $I->assertEquals($response, 'resend email');
+    }
+
+    public function testActivateUserAfterSignup(FunctionalTester $I) {
+        $I->wantTo('activate user after sign up');
+        $userId = $I->grabFromDatabase('user', 'id', [
+            'email' => '123@mail.com',
+        ]);
+        $activationToken = $I->grabFromDatabase('user_token', 'token', [
+            'user_id' => $userId,
+            'action' => 1,
+        ]);
+        $I->sendPOST('user/activate', [
+            'token' => $activationToken,
+            'email' => '123@mail.com',
+        ]);
+        $I->seeResponseCodeIs(200);
+        $response = json_decode($I->grabResponse());
+        $I->seeResponseMatchesJsonType([
+          'token' => 'string',
+          'fullname' => 'string',
+        ]);
+        $this->accessToken = $response->token;
+        $I->seeInDatabase('user', [
+            'fullname' => 'Tan Ah Boy',
+            'role' => 10,
+            'status' => 10,
+        ]);
+    }    
 
 }
--- tests/codeception/api/functional/UserCest.php
+++ tests/codeception/api/functional/UserCest.php
@@ -36,6 +36,43 @@ class UserCest
         ]);
     }
 
+    public function testChangePassword(FunctionalTester $I)
+    {
+        $token = $I->login('1111', '123456')->token;
+        $I->wantTo('change password');
+        $I->amBearerAuthenticated($token);
+        $I->sendPOST('user/change-password', [
+            'oldPassword' => '123456',
+            'newPassword' => '654321',
+        ]);
+        $I->seeResponseCodeIs(200);
+        $response = json_decode($I->grabResponse());
+        $I->assertEquals($response, 'change password successfully');
+
+        $I->sendPOST('user/change-password', [
+            'oldPassword' => '654321',
+            'newPassword' => '123456',
+        ]);
+    } 
+
+    public function testChangeEmail(FunctionalTester $I)
+    {
+        $token = $I->login('1111', '123456')->token;
+        $I->wantTo('change email');
+        $I->amBearerAuthenticated($token);
+        $I->sendPOST('user/change-email', [
+            'newEmail' => '123456@mail.com',
+            'password' => '123456',
+        ]);
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseMatchesJsonType([
+            'email' => 'string',
+        ]);
+        $I->seeResponseContainsJson([
+            'email' => '123456@mail.com',
+        ]);
+    } 
+
     public function testLogout(FunctionalTester $I)
     {
         $token = $I->login('1111', '123456')->token;
@@ -44,4 +81,22 @@ class UserCest
         $I->sendGET('user/logout');
         $I->seeResponseCodeIs(200);
     }    
+
+    public function testChangeMobilePhone(FunctionalTester $I)
+    {
+        $token = $I->login('1111', '123456')->token;
+        $I->wantTo('change mobile phone');
+        $I->amBearerAuthenticated($token);
+        $I->sendPOST('user/change-mobile', [
+            'newMobile' => '88888888',
+            'password' => '123456',
+        ]);
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseMatchesJsonType([
+            'mobile' => 'string',
+        ]);
+        $I->seeResponseContainsJson([
+            'mobile' => '88888888',
+        ]);
+    } 
 }
